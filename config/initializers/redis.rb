835e096e187838f17a84f7fc3eece828af75f43a
Shawn Deprey <shawndeprey@gmail.com>
2013-07-13 12:44:59 -0700

    Trying to get redis to work.

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,5 @@
 if Rails.env.production?
-	uri = URI.parse(ENV["REDISCLOUD_URL"])
+	#uri = URI.parse(ENV["REDISCLOUD_URL"])
+	uri = URI.parse("redis://rediscloud:r12wd84fdA192cys@pub-redis-19517.us-east-1-3.2.ec2.garantiadata.com:19517")
 	$redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 end
