8e8c0b246d6b7841067e055de268db880f55e141
Shawn Deprey <shawndeprey@gmail.com>
2013-07-27 19:59:06 -0700

    Trying to get tool to work in prod. Index NOT working.......

--- app/assets/javascripts/spider.js
+++ app/assets/javascripts/spider.js
@@ -8,11 +8,19 @@
       },
       search: {
         init: function(){
+          /*
+            search_bar = $('body.v1 input#search_bar');
+          $('body.v1 button#search_button').off('click').on('click', function(){
+            console.log($(search_bar).val());
+            $.getJSON('/api/v1/species/search.json?q='+$(search_bar).val(), function(yield){
+              $('body.v1 span.loading').css('display', 'block');
+              spider.search.updateResults(yield);
+            });
+          });
+          */
           $('body.v1 input#search_bar').off('keyup change').on('keyup change', function(){
-            $.getJSON('/api/v1/species/search.json?q='+$(this).val(),{async: true}, function(yield){
-              setTimeout(function(){
-                spider.search.updateResults(yield);
-              },0);
+            $.getJSON('/api/v1/species/search.json?q='+$(this).val(), function(yield){
+              spider.search.updateResults(yield);
             });
           });
         },
@@ -23,9 +31,9 @@
           $resultCount.html(json.length);
           $.each(json, function(){
             imageSrc = "/images/spider-id-logo.png"
-            commonName = this.common_name || this.scientific_name
-            scientificName = this.scientific_name
-            characteristics = this.characteristics
+            commonName = (this.common_name || this.scientific_name) || 'N/A'
+            scientificName = this.scientific_name || 'N/A'
+            characteristics = this.characteristics || 'N/A'
             $results.append('\
               <div class="span6 card">\
                 <img alt="Spider id logo" src="'+imageSrc+'">\
